ddfa1cf5fe72cc0795aa8d854d349e5bea855154
DariusAlex03 <darius_buzura@yahoo.com>
2022-05-05 14:49:34 +0300

    ajout classe nombre

---
+++ src/main/Nombre.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Nombre {
+    private int valeurNombre;
+
+    public int Nombre(int nb){
+        return nb;
+    }
+
+    public int getValeurNombre() {
+        return valeurNombre;
+    }
+
+    public void setValeurNombre(int valeurNombre) {
+        this.valeurNombre = valeurNombre;
+    }
+}
