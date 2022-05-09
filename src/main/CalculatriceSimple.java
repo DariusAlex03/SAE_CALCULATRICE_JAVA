306618577b077b4e90f6d63882c1cf1edac257f1
DariusAlex03 <darius_buzura@yahoo.com>
2022-05-09 14:02:17 +0300

    version finale sans exceptions

--- src/main/CalculatriceSimple.java
+++ src/main/CalculatriceSimple.java
@@ -6,17 +6,24 @@ public class CalculatriceSimple {
         Multiplication m = new Multiplication(n, n);
         System.out.println(m.toString());
 
-        Nombre neuf = new Nombre(8);
-        Nombre huit = new Nombre(2);
+        Nombre huit = new Nombre(8);
+        Nombre deux = new Nombre(2);
 
-        Operation j = new Multiplication(neuf , huit);
+        Operation j = new Multiplication(huit , deux);
         System.out.println(j.toString());
 
+        Operation a = new Addition(huit, deux);
+        System.out.println(a.toString());
+
+        Operation s = new Soustraction(huit, deux);
+        System.out.println(s.toString());
+
+        Operation d = new Division(huit, deux);
+        System.out.println(d.toString());
 
 
-
-
-
-
+        Nombre zero = new Nombre(0);
+        Operation z = new Division(huit,zero);
+        System.out.println(z.toString());
     }
 }
--- src/main/Division.java
+++ src/main/Division.java
@@ -3,10 +3,17 @@ package main;
 public class Division extends Operation{
     public Division(Nombre op1, Nombre op2){
         super(op1, op2);
-
     }
     public int valeur(){
-        return getOPerande1().valeur()/ getOPerande2().valeur();
+        int result = 0;
+        try {
+            result = getOPerande1().valeur() / getOPerande2().valeur();
+            return result;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Attention, division par 0, valeur retourne automatiquement est 0");
+        }
+        return result;
     }
 
     public String toString() {
